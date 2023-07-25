17cfc10523c5ee436c0ba966778c9b7e86495435
GammaGammaRay <benjamin.peret@gmail.com>
2023-07-25 20:41:25 -0300

    feat: postgres config and .env

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const port = 5000
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port, () =>
   console.log(chalk.bold.green(`Server running on ${port}`))
-);
+)
---
+++ src/database/database.js
@@ -0,0 +1,12 @@
+import pg from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const { Pool } = pg
+
+const configDatabase = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export const db = new Pool(configDatabase)
